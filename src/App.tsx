6d922aa40443b7640b8118102aaa4f19f765d907
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-12-22 14:19:15 -0300

    Criar Componente Cadastrar Usuario e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
     return (
         <>
             <Navbar />
-            <Home />
+            {/* <Home /> */}
+            {/* <Login /> */}
+            <Cadastro />
             <Footer />
         </>
     )

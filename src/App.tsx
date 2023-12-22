ae56fa96358815bb9707ba8cad17b570f6cb9884
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-12-22 14:06:11 -0300

    Criar os Componentes Navbar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 
 function App() {
     return (
-        <Home />
+        <>
+            <Navbar />
+            <Home />
+            <Footer />
+        </>
     )
 }
 
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+﻿
+function Navbar() {
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+            
+                <div className="container flex justify-between text-lg">
+                    Blog Pessoal
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar

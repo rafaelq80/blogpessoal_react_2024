7e3a4348c10c241b5442bd465e93400f161ef7cc
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-01-05 02:05:44 -0300

    Update Cadastro

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,8 +1,9 @@
-﻿import { useNavigate } from 'react-router-dom'
-import './Cadastro.css'
-import { useState, useEffect, ChangeEvent, FormEvent } from 'react'
+﻿import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Usuario from '../../models/Usuario'
+import './Cadastro.css'
 import { cadastrarUsuario } from '../../services/Service'
+import { RotatingLines } from 'react-loader-spinner'
 
 function Cadastro() {
 
@@ -31,6 +32,8 @@ function Cadastro() {
 
   function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
     setConfirmaSenha(e.target.value)
+    console.log(usuario);
+    console.log(confirmaSenha);
   }
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
@@ -38,6 +41,8 @@ function Cadastro() {
       ...usuario,
       [e.target.name]: e.target.value
     })
+
+    console.log(usuario);
   }
 
   async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>) {
@@ -58,6 +63,7 @@ function Cadastro() {
       alert('Dados inconsistentes. Verifique as informações de cadastro.')
       setUsuario({ ...usuario, senha: "" })
       setConfirmaSenha("")
+      console.log(usuario);
     }
 
     setIsLoading(false)
@@ -67,7 +73,8 @@ function Cadastro() {
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
         <div className="fundoCadastro hidden lg:block"></div>
-        <form className='flex justify-center items-center flex-col w-2/3 gap-3' >
+        <form className='flex justify-center items-center flex-col w-2/3 gap-3'
+          onSubmit={cadastrarNovoUsuario}>
           <h2 className='text-slate-900 text-5xl'>Cadastrar</h2>
           <div className="flex flex-col w-full">
             <label htmlFor="nome">Nome</label>
@@ -77,7 +84,8 @@ function Cadastro() {
               name="nome"
               placeholder="Nome"
               className="border-2 border-slate-700 rounded p-2"
-
+              value={usuario.nome}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
           <div className="flex flex-col w-full">
@@ -88,6 +96,8 @@ function Cadastro() {
               name="usuario"
               placeholder="Usuario"
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
           <div className="flex flex-col w-full">
@@ -98,6 +108,8 @@ function Cadastro() {
               name="foto"
               placeholder="Foto"
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.foto}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
           <div className="flex flex-col w-full">
@@ -108,6 +120,8 @@ function Cadastro() {
               name="senha"
               placeholder="Senha"
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
           <div className="flex flex-col w-full">
@@ -118,11 +132,14 @@ function Cadastro() {
               name="confirmarSenha"
               placeholder="Confirmar Senha"
               className="border-2 border-slate-700 rounded p-2"
+              value={confirmaSenha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
             />
           </div>
 
           <div className="flex justify-around w-full gap-8">
-            <button className='rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2'
+            <button className='rounded text-white bg-red-400 
+                              hover:bg-red-700 w-1/2 py-2'
               onClick={retornar}
             >
               Cancelar
@@ -130,9 +147,17 @@ function Cadastro() {
             <button
               type='submit'
               className='rounded text-white bg-indigo-400 
-                           hover:bg-indigo-900 w-1/2 py-2'
+                           hover:bg-indigo-900 w-1/2 py-2 
+                           flex justify-center'
             >
-              Cadastrar
+              {isLoading ? <RotatingLines
+                strokeColor="white"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="24"
+                visible={true}
+              /> :
+                <span>Cadastrar</span>}
             </button>
           </div>
 

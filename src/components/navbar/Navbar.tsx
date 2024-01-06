a7cc3cb85ceaef760c5ed2e011699497963304a3
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-01-05 23:33:45 -0300

    Update Context

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,32 +1,19 @@
-﻿import { useContext } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContext"
-
-function Navbar() {
-
-    const navigate = useNavigate()
-    const { handleLogout } = useContext(AuthContext)
-  
-    function logout() {
-        handleLogout()
-        alert('Usuário deslogado com sucesso')
-        navigate('/login')
-    }
-    
+﻿
+function Navbar() {    
     return (
         <>
             <div className='w-full bg-indigo-900 text-white
                 flex justify-center py-4'>
             
                 <div className="container flex justify-between text-lg">
-                    <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
+                    Blog Pessoal
 
                     <div className='flex gap-4'>
                         Postagens
                         Temas
                         Cadastrar tema
                         Perfil
-                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                        Sair
                     </div>
                 </div>
             </div>

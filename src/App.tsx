3e94cb45ea3a29b78688c7e83d28c8e244b6ea42
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-01-26 19:42:58 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import { AuthProvider } from './contexts/AuthContext'
+import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
   return (
     <>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -21,6 +23,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }

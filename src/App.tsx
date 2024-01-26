42405514fc46c759e4b431635ffe9e4067a5572d
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-01-26 19:42:08 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -21,6 +23,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }

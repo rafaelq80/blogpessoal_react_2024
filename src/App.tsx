0ac30a647cf6436fc1522f869df1d58e385c1c0e
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-01-26 18:50:37 -0300

    Criar Login

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
@@ -10,11 +11,12 @@ function App() {
     <>
         <BrowserRouter>
           <Navbar />
-          <div className="min-h-[77vh]">
+          <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
           <Footer />

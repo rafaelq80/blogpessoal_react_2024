34abe6bd7daf77ab9e7557d2547a18cdf513ed8a
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-03-04 10:28:50 -0300

    update

--- src/components/temas/cardtemas/CardTemas.tsx
+++ src/components/temas/cardtemas/CardTemas.tsx
@@ -18,8 +18,9 @@ function CardTema({ tema }: CardTemasProps) {
                     <button>Editar</button>
                 </Link>
 
-                <Link to='' className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
-                    flex items-center justify-center'>
+                <Link to='' 
+                    className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
+                        flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
             </div>

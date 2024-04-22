4e91e5c16e21fee47edf4be8c7e97e719f0ac073
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-04-22 12:24:37 -0300

    Update

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,8 +1,8 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'https://blogpessoal.onrender.com'
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://blog-spring-2024.onrender.com'
+    //baseURL: 'http://localhost:4000'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) =>{

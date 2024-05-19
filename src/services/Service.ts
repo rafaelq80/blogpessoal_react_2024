503e73d41ea5c85f4cef2822fdcbe2eef304c338
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-05-19 11:31:52 -0300

    Deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,8 +1,7 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blog-spring-2024.onrender.com'
-    //baseURL: 'http://localhost:4000'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) =>{

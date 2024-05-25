06bc6dfbdc6ff60ad13af97cfb9d6393dc37781b
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-05-25 04:00:37 -0300

    Update

--- src/components/temas/listatemas/ListaTemas.tsx
+++ src/components/temas/listatemas/ListaTemas.tsx
@@ -22,7 +22,6 @@ function ListaTemas() {
             })
         } catch (error: any) {
             if (error.toString().includes('403')) {
-                alert('O token Expirou!')
                 handleLogout()
             }
         }
@@ -36,31 +35,27 @@ function ListaTemas() {
     }, [token])
 
     useEffect(() => {
-        buscarTemas()    
+        buscarTemas()
     }, [temas.length])
-    
+
     return (
         <>
-        {temas.length === 0 && (
-            <DNA
-            visible={true}
-            height="200"
-            width="200"
-            ariaLabel="dna-loading"
-            wrapperStyle={{}}
-            wrapperClass="dna-wrapper mx-auto"
-        />
-        )}
+            {temas.length === 0 && (
+                <DNA
+                    visible={true}
+                    height="200"
+                    width="200"
+                    ariaLabel="dna-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="dna-wrapper mx-auto"
+                />
+            )}
             <div className="flex justify-center w-full my-4">
                 <div className="container flex flex-col">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                        <>
-                            {temas.map((tema) => (
-                                <>
-                                    <CardTema key={tema.id} tema={tema} />
-                                </>
-                            ))}
-                        </>
+                        {temas.map((tema) => (
+                            <CardTema key={tema.id} tema={tema} />
+                        ))}
                     </div>
                 </div>
             </div>

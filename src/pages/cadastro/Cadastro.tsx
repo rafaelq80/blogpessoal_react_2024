4e319e0f138b74e967ae48e18d973853795411e0
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-05-29 11:45:23 -0300

    Update

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -5,6 +5,7 @@ import { RotatingLines } from 'react-loader-spinner'
 import Usuario from '../../models/Usuario'
 
 import './Cadastro.css'
+import { ToastAlerta } from '../../utils/ToastAlerta'
 
 function Cadastro() {
 
@@ -53,12 +54,12 @@ function Cadastro() {
 
       try {
         await cadastrarUsuario(`/usuarios/cadastrar`, usuario, setUsuario)
-        alert('Usuário cadastrado com sucesso!')
+        ToastAlerta('Usuário cadastrado com sucesso!', 'sucesso')
       } catch (error) {
-        alert('Erro ao cadastrar o usuário!')
+        ToastAlerta('Erro ao cadastrar o usuário!', 'erro')
       }
     } else {
-      alert('Dados estão inconsistentes. Verifique as informações do cadastro')
+      ToastAlerta('Dados estão inconsistentes. Verifique as informações do cadastro', 'erro')
       setUsuario({ ...usuario, senha: '' })
       setConfirmaSenha('')
     }
